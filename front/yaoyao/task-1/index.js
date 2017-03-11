1348642b2e740e3eeb558b8e8a1e10266a28dbe0
SmarkSeven <wuziq@vip.qq.com>
2017-03-12 03:55:32 +0800

    添加字符串长度处理函数

--- front/yaoyao/task-1/index.js
+++ front/yaoyao/task-1/index.js
@@ -1,4 +1,19 @@
 ;(function(){
+  
+ var getStrLen =  function(str){
+     var enLen = 0;
+     var zhLen = 0;
+     for (let ch of str) {
+     if (isASCII(ch))
+        enLen++;
+     else 
+        zhLen++;
+      } 
+     return enLen + zhLen * 2;  
+  }
+  var isASCII = function (c) {
+      return c.codePointAt(0) <= 0xFF;
+  }
   var inputEl = document.getElementById('input');
   var btn = document.getElementById('btn');
   var tipEl = document.getElementById('tip-tool');
@@ -6,7 +21,8 @@
   btn.addEventListener('click',function(){
     var inputContent = inputEl.value && inputEl.value.trim();
     if(inputContent){
-      if((inputContent.length >= 4) && (inputContent.length <= 16)){
+      var len = getStrLen(inputContent)
+      if((len >= 4) && (len <= 16)){
        tipEl.textContent = '格式正确';
        inputEl.classList.remove('input-error'); 
        tipEl.classList.remove('tip-error'); 
